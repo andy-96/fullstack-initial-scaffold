f559d4ed4478cb8c41d5fe1ebf3aa4e0b9f5e426
Andy Chen <mail@andychen.de>
2021-02-18 18:58:00 +0100

    initial scaffold

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template lang='pug'>
+home
+</template>
+
+<script>
+import Home from './Views/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    home: Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
